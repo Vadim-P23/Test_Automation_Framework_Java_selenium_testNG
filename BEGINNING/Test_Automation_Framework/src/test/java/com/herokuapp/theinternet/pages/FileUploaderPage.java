41cf024f105efecda4d5ecd8a1cafbbda8759428
Vadim-P23 <petrov.vadim.qa@gmail.com>
2023-04-14 16:24:34 -0400

    added changes

--- BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/pages/FileUploaderPage.java
+++ BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/pages/FileUploaderPage.java
@@ -1,6 +1,41 @@
 package com.herokuapp.theinternet.pages;
 
-public class FileUploaderPage {
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class FileUploaderPage extends BasePageObject{
+
+    public FileUploaderPage(WebDriver driver, Logger log) {
+        super(driver, log);
+    }
+    private final String pageUrl = "https://the-internet.herokuapp.com/upload";
+    private final By chooseFile = By.id("file-upload");
+    private final By upload = By.id("file-submit");
+    private final By uploadedFiles = By.id("uploaded-files");
+
+
+    public void openPage() {
+        log.info("Opening page " + pageUrl);
+        openUrl(pageUrl);
+    }
+
+    public void pushUploadButton() {
+        click(upload);
+    }
+
+    public void selectingFiles(String fileName) {
+        String filePath = System.getProperty("user.dir") + "//src//main//resources//files//text" + fileName;
+        type(filePath, chooseFile);
+    }
+
+    public  String getUploadedFilesNames() {
+        return getText(uploadedFiles);
+    }
+
+
+
+
 
 
 }
