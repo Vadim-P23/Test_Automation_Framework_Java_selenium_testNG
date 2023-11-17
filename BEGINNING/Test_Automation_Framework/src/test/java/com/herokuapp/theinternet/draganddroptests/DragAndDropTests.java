e040b8627138d19b888e5d91e7373dc53750402b
Vadim-P23 <petrov.vadim.qa@gmail.com>
2023-11-17 10:42:48 -0500

    done

--- BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/draganddroptests/DragAndDropTests.java
+++ BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/draganddroptests/DragAndDropTests.java
@@ -3,13 +3,27 @@ package com.herokuapp.theinternet.draganddroptests;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.DragAndDropPage;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DragAndDropTests extends TestUtilities {
+
     @Test
     public void dragAtoBtest() {
         log.info("Starting dragAtoBtest");
+
         DragAndDropPage dragAndDropPage = new DragAndDropPage(driver, log);
+        dragAndDropPage.openPage();
+        dragAndDropPage.dragAtoB();
+
+        // Verify correct headers in correct boxes
+        String columnAText = dragAndDropPage.getColumnAText();
+        Assert.assertEquals(columnAText, "B", "Column A Header should be B");
+        String columnBText = dragAndDropPage.getColumnBText();
+        Assert.assertEquals(columnBText, "A", "Column B Header should be A");
+
+
+
     }
 
 }
--- BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/pages/DragAndDropPage.java
+++ BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/pages/DragAndDropPage.java
@@ -1,13 +1,45 @@
 package com.herokuapp.theinternet.pages;
 
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
 public class DragAndDropPage extends BasePageObject {
 
+    private String pageUrl = "https://the-internet.herokuapp.com/drag_and_drop";
+
+    private By column_a = By.id("column-a");
+    private By column_b = By.id("column-b");
+
     public DragAndDropPage(WebDriver driver, Logger log) {
         super(driver, log);
     }
+
+    public void openPage() {
+        log.info("Open drag and drop page");
+        openUrl(pageUrl);
+        log.info("Drag and drop page was opened!");
+    }
+
+    public void dragAtoB() {
+        log.info("Drag and drop A box on b box");
+        performDragAndDrop(column_a, column_b);
+    }
+    /** Getting Column A Text */
+    public String getColumnAText() {
+        String text = find(column_a).getText();
+        log.info("Column A Text: " + text);
+        return text;
+    }
+
+    /** Getting Column B Text */
+    public String getColumnBText() {
+        String text = find(column_b).getText();
+        log.info("Column B Text: " + text);
+        return text;
+    }
+
+
 
 }
