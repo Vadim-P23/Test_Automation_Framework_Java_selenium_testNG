9ab0aea8d55905e1c66d7720178a2b74f92bb9c7
Vadim-P23 <petrov.vadim.qa@gmail.com>
2023-11-29 11:32:39 -0500

    hovers

--- BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/pages/HoversPage.java
+++ BEGINNING/Test_Automation_Framework/src/test/java/com/herokuapp/theinternet/pages/HoversPage.java
@@ -1,2 +1,30 @@
-package com.herokuapp.theinternet.pages;public class HoversPage {
+package com.herokuapp.theinternet.pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class HoversPage extends BasePageObject {
+
+    public HoversPage(WebDriver driver, Logger log) {
+        super(driver, log);
+    }
+
+    private By hovers = By.xpath("//a[.='Hovers']");
+    private By usersAvatars = By.xpath("//div[@class='figure']/img");
+    private By viewProfiles = By.xpath("//a[.='View profile']");
+
+    public void openHoversPage() {
+        click(hovers);
+    }
+
+    public void openUserProfile(int i) {
+        List<WebElement> avatars = findAll(usersAvatars);
+        hoverOverElement(avatars.get(i - 1));
+        List<WebElement> profiles = findAll(viewProfiles);
+        profiles.get(i - 1).click();
+    }
 }
